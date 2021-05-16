0aa2b192e4d3673ea471b19a6bb62db84f09db1b
alan20210202 <little_alanma@163.com>
2021-05-16 20:52:21 +0800

    Parallelize processing.

--- main.py
+++ main.py
@@ -20,9 +20,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. """
 
 import base64
+import glob
 import io
 import os
-from typing import Optional, Generator, Union, Tuple
+from collections.abc import Iterable
+from itertools import repeat
+from multiprocessing import Pool
+from pathlib import Path
+from typing import Optional, Union, Tuple
 
 import click
 import fitz
@@ -34,17 +39,71 @@ from tqdm import tqdm
 StrPath = Union[str, os.PathLike]
 
 
-def clean_doc_selenium(images: Generator[Tuple[bytes, str], None, None]) \
-        -> Generator[Image.Image, None, None]:
+def merge_to_pdf(pages: Iterable[Union[Image.Image, bytes]], output: StrPath):
     """
-    Cleans the scanned document pages using docleaner's online service.
+    Converts and merges images to a one-page pdf file, performing optional
+    OCR in the process.
 
-    :param images: A generator yielding paths to document pages.
+    :param pages: A generator yielding PIL image objects.
+    :param output: Path to the result pdf.
     """
-    for (image, ext) in images:
+    doc = fitz.Document()
+    for page in pages:
+        if isinstance(page, Image.Image):
+            # noinspection PyUnresolvedReferences
+            doc_page = doc.new_page(width=page.width, height=page.height)
+            buffer = io.BytesIO()
+            page.save(buffer, format="jpeg")
+            doc_page.insert_image(fitz.Rect(0, 0, page.width, page.height),
+                              stream=buffer)
+        else:
+            page = fitz.Document(stream=page, filetype="pdf")
+            doc.insert_pdf(page)
+    doc.save(output)
+
+
+def convert_pdf_page_to_image(pdf: StrPath, idx: int, dpi: int) -> bytes:
+    """
+    Converts a PDF page to an image.
+
+    :param pdf: Path to the PDF.
+    :param idx: Page index (0-offset).
+    :param dpi: Pixel density. A value > 200 is recommended.
+    :return: Raw image as bytes.
+    """
+    doc = fitz.Document(pdf)
+    matrix = fitz.Matrix(dpi / 72, dpi / 72)
+    assert 0 <= idx < doc.page_count
+    # noinspection PyUnresolvedReferences
+    return doc[idx].get_pixmap(matrix=matrix).tobytes()
+
+
+def clean_single_page(args: Tuple[StrPath, int, int, bool, bool]) \
+        -> Union[Image.Image, bytes]:
+    """
+    Cleans a single page.
+
+    :param args: A tuple consisting of (in order):
+        1. Path to the page (pdf or image),
+        2. Index (image index or page index in PDF),
+        3. DPI (-1 if an image is direcly supplied),
+        4. Whether to perform OCR,
+        5. Whether to actually clean the page.
+    :return:  If OCR is enabled, a OCR-ed PDF in raw bytes, otherwise an PIL
+        Image object representing the cleaned page.
+    """
+    page, idx, dpi, ocr, clean = args
+    if dpi > 0:
+        image = convert_pdf_page_to_image(page, idx, dpi)
+        ext = ".png"
+    else:
+        with open(page, "rb") as file:
+            image = file.read()
+        ext = os.path.splitext(page)[1]
+    if clean:
         # noinspection HttpUrlsUsage
         req = requests.post("http://service.docleaner.cn/attachCollect/upload",
-                            files={"file": (f"image.{ext}", image)})
+                            files={"file": (f"image{ext}", image)})
         data = {
             # Weird typo in the API.
             "paramers": "降噪,去斑点,去黑边,去背景,自动纠斜",
@@ -54,79 +113,17 @@ def clean_doc_selenium(images: Generator[Tuple[bytes, str], None, None]) \
         }
         # noinspection HttpUrlsUsage
         req = requests.post("http://service.docleaner.cn/exe/daqw", data=data)
-        result = base64.b64decode(req.json()["data"]["outFileStr"])
-        yield Image.open(io.BytesIO(result))
-
-
-def convert_pdf_to_images(pdf: StrPath, fmt: str, dpi: int,
-                          output: Optional[StrPath] = None,
-                          first_page: Optional[int] = None,
-                          last_page: Optional[int] = None) \
-        -> Generator[Tuple[bytes, str], None, None]:
-    """
-    Converts a pdf file to images. This a necessary pre-processing step
-    because docleaner online only accepts images as inputs.
-
-    :param pdf: The path to the pdf file.
-    :param fmt: Image file format. jpg is the fastest but not lossless; png is
-        lossless but slow; tiff is theoretically the best but occupies a lot of
-        disk space.
-    :param dpi: Pixel density of the output image.
-    :param output: The output directory of intermediate images.
-    :param first_page: First page to convert (starting from 1, inclusive).
-    :param last_page: Last page to convert (starting from 1, inclusive).
-    :return: A generator yielding (image as raw bytes, image format).
-    """
-    doc = fitz.Document(pdf)
-
-    matrix = fitz.Matrix(dpi / 72, dpi / 72)
-    first_page = 0 if first_page is None else first_page - 1
-    last_page = doc.page_count if last_page is None else last_page
-    for i in range(first_page, last_page):
-        # noinspection PyUnresolvedReferences
-        data = doc[i].get_pixmap(matrix=matrix).tobytes()
-        if output is not None:
-            filename = os.path.join(output, f"{i}.{fmt}")
-            with open(filename, "wb") as file:
-                file.write(data)
-        yield data, "png"
-
-
-def convert_images_to_pdf(images: Generator[Image.Image, None, None],
-                          output: StrPath,
-                          ocr: bool = True, total: Optional[int] = None):
-    """
-    Converts and merges images to a one-page pdf file, performing optional
-    OCR in the process.
-
-    :param images: A generator yielding PIL image objects.
-    :param output: Path to the result pdf.
-    :param ocr: Whether to perform OCR(Optical Character Recognition).
-    :param total: An optional integer hinting the total number of images given.
-        If supplied, a progress bar will be displayed during the conversion.
-    """
-    doc = fitz.Document()
-    for image in images if total is None else tqdm(images, total=total):
-        if ocr:
-            pdf = pytesseract.image_to_pdf_or_hocr(image)
-            page = fitz.Document(stream=pdf, filetype="pdf")
-            doc.insert_pdf(page)
-        else:
-            # noinspection PyUnresolvedReferences
-            page = doc.new_page(width=image.width, height=image.height)
-            buffer = io.BytesIO()
-            image.save(buffer, format="jpeg")
-            page.insert_image(fitz.Rect(0, 0, image.width, image.height),
-                              stream=buffer)
-    doc.save(output)
+        image = base64.b64decode(req.json()["data"]["outFileStr"])
+    image = Image.open(io.BytesIO(image))
+    if ocr:
+        return pytesseract.image_to_pdf_or_hocr(image)
+    return image
 
 
 # noinspection PyShadowingBuiltins
 @click.command()
-@click.argument("input", type=click.Path(exists=True))
+@click.argument("input", type=click.Path())
 @click.argument("output", type=click.Path())
-@click.option("-f", "--format", default="png",
-              help="Intermediate image format.")
 @click.option("-d", "--dpi", default=300, help="DPI for rasterization.")
 @click.option("--first-page", type=int, help="First page to convert/clean.")
 @click.option("--last-page", type=int, help="Last page to convert/clean.")
@@ -134,17 +131,42 @@ def convert_images_to_pdf(images: Generator[Image.Image, None, None],
               help="Whether to perform OCR during the conversion.")
 @click.option("--clean/--dont-clean", default=True,
               help="Whether to clean pdf using docleaner's online service.")
-def main(input: str, output: str, format: str, dpi: int,
+def main(input: str, output: str, dpi: int,
          first_page: Optional[int], last_page: Optional[int], ocr: bool,
          clean: bool):
-    images = convert_pdf_to_images(input, fmt=format, dpi=dpi,
-                                   first_page=first_page, last_page=last_page)
-    if clean:
-        images = clean_doc_selenium(images)
-    doc = fitz.Document(input)
-    total = (doc.page_count if last_page is None else last_page) \
-        - (0 if first_page is None else first_page - 1)
-    convert_images_to_pdf(images, output, ocr=ocr, total=total)
+    if os.path.splitext(input)[1].lower() == ".pdf":
+        # PDF mode
+        assert os.path.exists(input)
+        page_count = fitz.Document(input).page_count
+        first_page = 0 if first_page is None else first_page - 1
+        last_page = page_count if last_page is None else last_page
+        args = zip(repeat(input), range(first_page, last_page),
+                   repeat(dpi), repeat(ocr), repeat(clean))
+    else:
+        # Glob mode
+        files = sorted(glob.glob(input, recursive=True))
+        first_page = 0 if first_page is None else first_page - 1
+        last_page = len(files) if last_page is None else last_page
+        args = zip(files[first_page:last_page], repeat(0), repeat(-1),
+                   repeat(ocr), repeat(clean))
+    total = last_page - first_page
+    with Pool() as p:
+        results = tqdm(p.imap(clean_single_page, args), total=total)
+        if os.path.splitext(output)[1].lower() == ".pdf":
+            merge_to_pdf(results, output)
+        elif not os.path.exists(output) or os.path.isdir(output):
+            if ocr:
+                raise RuntimeError("the OCR flag is useless because we are "
+                                   "writing images (not PDF) to the output "
+                                   "directory.")
+            if not os.path.exists(output):
+                Path(output).mkdir(parents=True)
+            for (index, page) in enumerate(results):
+                file_path = os.path.join(output, f"{index}.jpg")
+                assert isinstance(page, Image.Image)
+                page.save(file_path)
+        else:
+            raise RuntimeError("invalid output format.")
 
 
 if __name__ == "__main__":
